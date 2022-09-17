b4051e80793dc0f64929a839638028f67def20dc
BarkMulcher <lucas.c.haskell@gmail.com>
2022-09-17 19:22:52 -0400

    initial commit

---
+++ Develop/db/server.js
@@ -0,0 +1,72 @@
+// import express module
+const express = require('express');
+// built-in path module:
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data:
+app.use(express.json());
+
+app.use(express.static('public'));
+
+// GET route for notes.html:
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+);
+
+// wildcard GET route for index.html (homepage):
+app.get('/*', (req, res) =>
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+);
+
+// GET /api/notes to read db.json file
+app.get('/api/notes', (req, res) => {
+    const response = JSON.parse(
+        fs.readFileSync('/db/db.json').toString())
+        res.send( response );
+    });  
+
+// POST /api/notes receive & add to the GUI w/ uuid
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+    // make sure there is data to enter:
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        }
+
+        readAndAppend(newNote, './db/db.json');
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.json(response);
+    } else {
+        res.json('Error in posting note');
+    }
+})
+
+// assign port:
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+);
+
+// You haven’t learned how to handle DELETE requests, but this 
+//application has that functionality in the front end. As a bonus,
+// see if you can add the DELETE route to the application using 
+//the following guideline:
+
+// DELETE /api/notes/:id should receive a query parameter containing
+// the id of a note to delete. In order to delete a note, you'll need 
+// to read all notes from the db.json file, remove the note with the
+// given id property, and then rewrite the notes to the db.json file.
+
+
--- Develop/public/assets/scripts/notes.js
+++
@@ -1,9 +0,0 @@
-// notes form
-const notesTitle = document.getElementsByClassName('note-title');
-const notesBody = document.getElementsByClassName('notes-textarea');
-const submitBtn = document.getElementById('submit-btn');
-
-submitBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.location.href = '/';
-});
