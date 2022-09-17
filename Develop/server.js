036e3a606968aa18bde6d7352cfa773fad011b94
BarkMulcher <lucas.c.haskell@gmail.com>
2022-09-17 17:59:29 -0400

    2nd commit - omit files & edit JS

--- Develop/server.js
+++ Develop/server.js
@@ -15,15 +15,22 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
+// GET route for notes.html:
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
 // wildcard GET route for index.html (homepage):
 app.get('/*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET route for notes.html:
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+// GET /api/notes to read db.json file
+app.get('/api/notes', (req, res) => {
+    const response = JSON.parse(
+        fs.readFileSync('/db/db.json').toString())
+        res.send( response );
+    });  
 
 // assign port:
 app.listen(PORT, () =>
